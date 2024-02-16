a17af57b7ff1ca80b79f3a1b427629fdd31483bb
Gio <86983840+gioaprilino@users.noreply.github.com>
2024-02-17 06:19:07 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-rm -rf h870/* h872/* us997/* 
-rm crdroid10.1/*.zip
-crave pull cd9/out/target/product/*/*.zip cd9/out/target/product/*/recovery.img
-mv h870/recovery.img h870/recoveryh870.img
-mv h872/recovery.img h872/recoveryh872.img
-mv us997/recovery.img us997/recoveryus997.img
-mv h870/* h872/* us997/* ./crdroid10.1/ 
-cd crdroid10.1
+rm -rf mojito/* 
+rm upload/*.zip
+crave pull out/target/product/*/*.zip out/target/product/*/recovery.img
+mv mojito/*  ./upload/ 
+cd upload
 ./multi_upload.sh
